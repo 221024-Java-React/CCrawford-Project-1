2f994c8b3bc6ffe5444943e235497b80fe4d20f0
christopherc360 <chriscrawford360@Outlook.com>
2022-10-25 11:56:48 -0500

    Lab2

---
+++ labs/Lab2.java
@@ -0,0 +1,29 @@
+package Labs;
+
+public class Lab2 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(add(15, 32));
+        System.out.println(sub(15, 32));
+        System.out.println(multiply(15.15, 32.32));
+        System.out.println(divide(15.15, 32.32));
+
+    }
+
+    public static int add(int x, int y){
+        return x + y;
+    }
+
+    public static int sub(int x, int y){
+        return x - y;
+    }
+
+    public static double multiply(double x, double y){
+        return x * y;
+    }
+
+    public static double divide(double x, double y){
+        return x / y;
+    }
+}
