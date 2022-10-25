4ad12a224f404f99120546611352f493c3f67893
christopherc360 <chriscrawford360@Outlook.com>
2022-10-25 11:44:19 -0500

    Lab1

---
+++ labs/Lab1.java
@@ -0,0 +1,30 @@
+package Labs;
+
+public class Lab1 {
+
+    public static void main(String[] args) {
+        
+        boolean boo = true;
+
+        byte maxByte = 127;
+
+        short minShort = -32_768;
+
+        int maxInt = 2_147_483_647;
+
+        long javaFlex = 5_500_000_000L;
+
+        float pie = 3.14159f;
+
+        double doubleYum = 3.14159 * 2;
+
+        System.out.print(boo);
+        System.out.println(maxByte);
+        System.out.println(minShort);
+        System.out.println(maxInt);
+        System.out.println(javaFlex);
+        System.out.println(pie);
+        System.out.println(doubleYum);
+
+    }
+}
